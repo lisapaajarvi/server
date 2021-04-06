603f9eda846cc5d5209e07506078bbaa1a5bd165
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-04-06 13:34:46 +0200

    added server

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-let colors = require('colors')
+const colors = require('colors');
+const http = require('http');
 
-console.log('Hello world'.rainbow)
+
+console.log('Hello world'.rainbow.bgWhite);
+console.log('Hello world'.green.underline);
+console.log('Hello world'.rainbow);
+
+const server = http.createServer((request, response) =>{
+    response.write('Hello World')
+    response.end('...')
+});
+
+server.listen(3000, 'localhost', ()=>{
+    //servern igång
+}
+)
